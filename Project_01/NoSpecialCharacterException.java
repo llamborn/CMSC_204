401c8e3a7d017c66422dd96685452b8c2984b9a8
Ⳑuke Ⳑamborn <pixel8@hotmail.com>
2026-03-04 17:12:32 -0500

    Add files via upload

---
+++ Project_01/NoSpecialCharacterException.java
@@ -0,0 +1,15 @@
+//Programmer: Luke L
+
+public class NoSpecialCharacterException extends Exception
+{
+
+	public NoSpecialCharacterException()
+	{
+		super("The password must contain at least one special character.");
+
+	}
+	public NoSpecialCharacterException(String message)
+	{
+		super(message);
+	}
+}
